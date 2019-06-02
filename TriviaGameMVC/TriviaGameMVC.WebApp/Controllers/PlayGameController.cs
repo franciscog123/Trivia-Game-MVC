d73d026b9f5c24b3f22c927a1bdaa1cde9514c25
Francisco Guerrero <guerrerof615@gmail.com>
2019-06-02 00:04:02 -0500

    moved link, working on displaying userquizzes

---
+++ TriviaGameMVC/TriviaGameMVC.WebApp/Controllers/PlayGameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TriviaGameMVC.WebApp.Models;
+
+namespace TriviaGameMVC.WebApp.Controllers
+{
+    public class PlayGameController : Controller
+    {
+        // GET: PlayGame
+        public ActionResult Index()
+        {
+            PlayGameViewModel model = new PlayGameViewModel();
+            
+            return View(model);
+        }
+    }
+}
